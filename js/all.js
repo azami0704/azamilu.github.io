c03509797b071de9990484e4bb814aebf2b98c91
unknown <doggy0704@gmail.com>
2023-03-11 00:55:10 +0800

    更新個人履歷,技能區塊

--- js/all.js
+++ js/all.js
@@ -4,9 +4,10 @@ const work = document.getElementById('work');
 const aboutTitle = document.querySelector('.about-title');
 const skillTitle = document.querySelector('.skill-title');
 const workTitle = document.querySelector('.work-title');
-const progressBar =document.querySelectorAll('.progress-bar')
-const skillSquare = document.querySelectorAll('.skill-list-item-square')
-const skillPercent = document.querySelectorAll('.skill-list-item-percent')
+const progressBar =document.querySelectorAll('.progress-bar');
+const skillSquare = document.querySelectorAll('.skill-list-item-square');
+const skillPercent = document.querySelectorAll('.skill-list-item-percent');
+const skillItemTitle = document.querySelectorAll('.skill-item-title');
 
 let progressExecuted = false;
 window.addEventListener('scroll',()=>{
@@ -21,33 +22,21 @@ window.addEventListener('scroll',()=>{
     }else{
         skillTitle.style.cssText="transform: translateY(-100%);opacity: 0;";
     }
+    if(window.scrollY>=skill.offsetTop-400){
+        skillItemTitle.forEach(item => {
+            item.style.cssText="transform: translateX(0);";
+        });
+    }else{
+        skillItemTitle.forEach(item => {
+            item.style.cssText="transform: translateX(-100%);";
+        });
+    }
     if(window.scrollY>=work.offsetTop-800){
         workTitle.style.cssText="transform: translateX(0);opacity: 1;";
+
     }else{
         workTitle.style.cssText="transform: translateX(-100%);opacity: 0;";
-    }
 
-    if(window.scrollY>=skill.offsetTop-700){
-        if(!progressExecuted){
-            progressBar.forEach((item,idx)=>{
-                let w=0;
-                let progress = setInterval(()=>{
-                    item.style.cssText=`width:${w}%`;
-                    skillPercent[idx].textContent=`${w}%`;
-                    skillSquare[idx].style.cssText=`transform: rotate(${45*w+45}deg);`;
-                    w++;
-                    if(w>progressBar[idx].dataset.width){
-                        clearInterval(progress);
-                    }
-                },10)
-            })
-        }
-        progressExecuted = true;
-    }else if(window.scrollY<skill.offsetTop-700){
-        progressBar.forEach(item=>{
-            item.style.width=`0%`;
-        })
-        progressExecuted = false;
     }
 })
 
